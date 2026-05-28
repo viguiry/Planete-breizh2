76c79f5d9c900e359b1f61958c1e0d1f6c5e0888
viguiry <viguiry@users.noreply.github.com>
2026-05-28 16:24:49 +0200

    Use separate checkout tunnel

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.PLANETE_BREIZH_CHECKOUT_ENDPOINT = "https://rasp2.tail0a90e.ts.net:8443/api/create-checkout-session";
+window.PLANETE_BREIZH_CHECKOUT_ENDPOINT = "https://simpsons-donated-develop-exceptional.trycloudflare.com/api/create-checkout-session";
