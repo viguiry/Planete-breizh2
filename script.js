1cb57c1db596f319b813c95cb12638484d8931c6
viguiry <viguiry@users.noreply.github.com>
2026-05-31 05:09:03 +0200

    Use real product photos

--- script.js
+++ script.js
@@ -86,7 +86,7 @@ function renderProducts() {
       (product) => `
         <article class="product-card">
           <div class="product-art">
-            <img class="product-photo" src="${product.image}?v=1" alt="${product.name} avec logo Planète Breizh" loading="lazy" />
+            <img class="product-photo" src="${product.image}?v=2" alt="${product.name} avec logo Planète Breizh" loading="lazy" />
           </div>
           <div class="product-content">
             <div>
